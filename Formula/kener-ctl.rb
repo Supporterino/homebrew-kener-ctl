9dcb91edfe904f6418d3fe11b5bc34af93f36eaf
Supporterino <lars@roth-kl.de>
2026-06-17 09:15:36 +0200

    fix: address brew audit warnings (license, sha256 placement)

--- Formula/kener-ctl.rb
+++ Formula/kener-ctl.rb
@@ -2,17 +2,18 @@ class KenerCtl < Formula
   desc "Declarative CLI for Kener status pages"
   homepage "https://github.com/supporterino/kener-ctl"
   version "0.1.0"
-  license "GPL-3.0"
+  license "GPL-3.0-only"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/supporterino/kener-ctl/releases/download/v#{version}/kener-ctl-darwin-arm64"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
     else
       url "https://github.com/supporterino/kener-ctl/releases/download/v#{version}/kener-ctl-darwin-x64"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
     end
   end
+
+  sha256 arm:   "0000000000000000000000000000000000000000000000000000000000000000",
+          intel: "0000000000000000000000000000000000000000000000000000000000000000"
 
   def install
     bin.install Dir["kener-ctl-*"].first => "kener-ctl"
